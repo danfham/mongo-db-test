e69c54edb088e302d86c42ed2df147d1300fb4d7
danfham <dan.cunningham91@icloud.com>
2023-07-15 10:23:43 -0400

    initialization of git repo

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+
+// terminal confirmation
+const PORT = process.env.PORT
+app.listen(PORT,console.log(`listening on port ${PORT}`))
